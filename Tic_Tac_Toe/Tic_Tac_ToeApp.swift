8c4de81cc03e871106ab31097928aa756a8c51a7
vuqt06 <vuqt@umich.edu>
2021-11-13 19:00:49 -0500

    Initial Commit

---
+++ Tic_Tac_Toe/Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_ToeApp.swift
+//  Tic_Tac_Toe
+//
+//  Created by Vu Trinh on 11/13/21.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
