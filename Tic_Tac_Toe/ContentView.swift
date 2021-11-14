3fb44270316650c5e0ca3bafce5afbbba1072bd4
vuqt06 <vuqt@umich.edu>
2021-11-13 21:19:43 -0500

    - Set up UI

--- Tic_Tac_Toe/ContentView.swift
+++ Tic_Tac_Toe/ContentView.swift
@@ -8,9 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    let columns = [GridItem(.flexible()),
+                   GridItem(.flexible()),
+                   GridItem(.flexible())];
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        GeometryReader {
+            geo in
+            VStack {
+                Spacer()
+                Text("Tic Tac Toe").font(Font.custom("asphyxiate", size: 30)).foregroundColor(.yellow)
+                    
+                Spacer()
+                LazyVGrid(columns: columns, spacing: 8) {
+                    ForEach(0..<9) {
+                        index in
+                        ZStack {
+                            Circle()
+                                .foregroundColor(.yellow)
+                                .frame(width: geo.size.width/3 - 10, height: geo.size.height/5 - 15)
+                                .cornerRadius(5)
+                            
+                            Image(systemName: "xmark")
+                                .resizable()
+                                .frame(width: 40, height: 40)
+                                .foregroundColor(.black)
+                        }
+                    }
+                }.padding([.horizontal], 8)
+                Spacer()
+            }.background(.black)
+                
+        }
+        .ignoresSafeArea()
     }
 }
 
